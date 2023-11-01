906c2a965e3a993e1ccfc271adafe7fc2f8c57e0
Clorster <58882718+Clorster@users.noreply.github.com>
2023-11-01 17:25:25 +0100

    json test

---
+++ scripts/app.js
@@ -0,0 +1,44 @@
+const title = document.querySelector('h1');
+const subtitle = document.querySelector('#subhead');
+const paragraph = document.querySelector('.info')
+
+const updateUI = (data) => {
+    const projectTitle = data.title;
+    const projectSubTitle = data.subtitle;
+    const projectparagraph = data.paragraph;
+
+
+
+    // update projectTitle template
+    title.innerHTML = `
+
+    ${projectTitle}
+    `
+
+    // update SubprojectTitle template
+    subtitle.innerHTML = `
+
+    ${projectSubTitle}
+    `
+
+    // update SubprojectTitle template
+    paragraph.innerHTML = `
+
+    ${projectparagraph}
+    `
+
+}
+
+const getProject = async (project) => {
+    const url = '../data/example.json';
+
+    const response = await fetch(url)
+    const data = await response.json();
+
+
+    return data;
+}
+
+getProject('project')
+  .then(data => updateUI(data))
+  .catch(err => console.log(err));
