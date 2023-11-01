683c129357a3961d5c96a19fda4fcbeea7db12e0
meldg0d <mankangodt@gmail.com>
2023-11-01 17:43:04 +0100

    json test with image

--- scripts/app.js
+++ scripts/app.js
@@ -1,31 +1,24 @@
 const title = document.querySelector('h1');
 const subtitle = document.querySelector('#subhead');
 const paragraph = document.querySelector('.info')
+const img = document.querySelector('.img')
 
 const updateUI = (data) => {
     const projectTitle = data.title;
     const projectSubTitle = data.subtitle; 
     const projectparagraph = data.paragraph;
-
-
+    const projectImage = data.image;
 
     // update projectTitle template
-    title.innerHTML = `
-
-    ${projectTitle}
-    `
+    title.innerHTML = `${projectTitle}`
 
     // update SubprojectTitle template
-    subtitle.innerHTML = `
-
-    ${projectSubTitle}
-    `
+    subtitle.innerHTML = `${projectSubTitle}`
 
     // update SubprojectTitle template
-    paragraph.innerHTML = `
+    paragraph.innerHTML = `${projectparagraph}`
+    img.setAttribute('src', projectImage);
 
-    ${projectparagraph}
-    `
 
 }
 
